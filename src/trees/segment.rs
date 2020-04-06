ecc8ee3d5e326eefec568d1cb24f2717f7dad93b
Kevin Zheng <kevinzhenng@gmail.com>
2020-04-06 17:59:47 -0400

    Use lambda

--- src/trees/segment.rs
+++ src/trees/segment.rs
@@ -13,22 +13,21 @@ macro_rules! right {
 #[derive(Debug)]
 pub struct SegmentTree<T: Copy + Default> {
     tree: Vec<T>,
-    comp: fn(T, T) -> T,
+    comb: fn(T, T) -> T,
     len: usize,
 }
 
 impl<T: Copy + Default> SegmentTree<T> {
-    fn new(arr: Vec<T>, comp: fn(T, T) -> T) -> SegmentTree<T> {
+    pub fn new(arr: Vec<T>, comb: fn(T, T) -> T) -> SegmentTree<T> {
         let mut backing = 1;
         while backing < arr.len() {
             backing <<= 1;
         }
         backing = 2 * backing - 1;
 
-        let default: T = Default::default();
         let mut s = SegmentTree {
-            tree: vec![default; backing],
-            comp,
+            tree: vec![Default::default(); backing],
+            comb,
             len: arr.len() as usize,
         };
         s.build(&arr, 0, 0, s.len - 1);
@@ -44,7 +43,7 @@ impl<T: Copy + Default> SegmentTree<T> {
         let mid = (start + end) / 2;
         self.build(arr, left!(node), start, mid);
         self.build(arr, right!(node), mid + 1, end);
-        self.tree[node] = (self.comp)(self.tree[left!(node)], self.tree[right!(node)]);
+        self.tree[node] = (self.comb)(self.tree[left!(node)], self.tree[right!(node)]);
     }
 
     fn range(&self, start: usize, end: usize) -> Option<T> {
@@ -72,7 +71,7 @@ impl<T: Copy + Default> SegmentTree<T> {
         match (l, r) {
             (_, None) => l,
             (None, _) => r,
-            (Some(lv), Some(rv)) => Some((self.comp)(lv, rv)),
+            (Some(lv), Some(rv)) => Some((self.comb)(lv, rv)),
         }
     }
 }
@@ -81,22 +80,16 @@ impl<T: Copy + Default> SegmentTree<T> {
 mod tests {
     use super::*;
 
-    fn add_u32(a: u32, b: u32) -> u32 {
-        a + b
-    }
-
     #[test]
-    fn create_segment_tree() {
+    fn segment_tree() {
         let arr = vec![1, 3, 5, 7, 9, 11];
         println!("Array: {:#?}", arr);
 
-        let tree = SegmentTree::new(arr, add_u32);
+        let tree = SegmentTree::new(arr, |a, b| a + b);
         println!("Tree: {:#?}", tree);
 
-        if let Some(sum) = tree.range(0, 1) {
+        if let Some(sum) = tree.range(0, 5) {
             println!("{}", sum);
-        } else {
-            println!("None");
         }
     }
 }
